a03e3299095182064ce4b77ce5cfcb0c5dc0dd4b
albodark10-tech <albodark10@gmail.com>
2026-07-30 20:30:04 -0600

    Set default PORT value in index.js
    Fix missing default port value in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,7 +29,7 @@ process.on('uncaughtException', (err) => {
 });
 
 const app  = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || ;
 
 // ── Trust Proxy ───────────────────────────────────────────────────
 app.set('trust proxy', 1);
